1bc172e2bb066c07efda8f324aff44fff19d23ab
nazari2079 <mohammadnazary2000@gmail.com>
2024-09-28 11:23:03 +0330

    feat: add eslint import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
 import jestDom from 'eslint-plugin-jest-dom';
 import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,6 +29,8 @@ export default [
       'plugin:jest-dom/recommended',
       'plugin:storybook/recommended',
       'prettier',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
     ),
   ),
   {
@@ -52,11 +54,49 @@ export default [
       react: {
         version: 'detect',
       },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
     },
 
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
       'no-console': ['error', { allow: ['error'] }],
+      'import/namespace': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
+          pathGroups: [
+            {
+              pattern: 'components',
+              group: 'internal',
+            },
+            {
+              pattern: 'common',
+              group: 'internal',
+            },
+            {
+              pattern: 'routes/ **',
+              group: 'internal',
+            },
+            {
+              pattern: 'assets/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['internal'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 ];
