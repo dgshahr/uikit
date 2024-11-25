14eccb4b368e4d02ec66b2e7a1960778ae5d4063
nazari2079 <mohammadnazary2000@gmail.com>
2024-11-25 10:07:05 +0330

    fix: fix show error and helper message in selectable inputs

--- src/components/Form/Wrappers/SelectableInputWrapper/SelectableInputWrapper.tsx
+++ src/components/Form/Wrappers/SelectableInputWrapper/SelectableInputWrapper.tsx
@@ -39,6 +39,7 @@ const SelectableInputWrapper: FC<SelectableInputWrapperProps> = (props) => {
   } = props;
 
   const icon = (props as SelectableInputBordered).icon;
+  const showError = isError || Boolean(errorMessage);
 
   return (
     <label
@@ -48,9 +49,9 @@ const SelectableInputWrapper: FC<SelectableInputWrapperProps> = (props) => {
           'dgs-ui-kit-p-3 dgs-ui-kit-border dgs-ui-kit-w-full dgs-ui-kit-border-solid dgs-ui-kit-rounded-xl dgs-ui-kit-bg-white has-[:checked]:dgs-ui-kit-bg-primary-50':
             variant === 'bordered',
           'has-[:checked]:dgs-ui-kit-border-primary-500 dgs-ui-kit-border-gray-200 hover:dgs-ui-kit-ring-4 hover:dgs-ui-kit-ring-gray-100 has-[:focus]:ring has-[:focus]:dgs-ui-kit-ring-primary-300 has-[:focus]:dgs-ui-kit-ring-offset-1 has-[:focus]:dgs-ui-kit-ring-offset-white':
-            variant === 'bordered' && !isError,
+            variant === 'bordered' && !showError,
           'dgs-ui-kit-border-error-500 hover:dgs-ui-kit-ring-4 hover:dgs-ui-kit-ring-error-50 has-[:focus]:ring has-[:focus]:dgs-ui-kit-ring-error-300 has-[:focus]:dgs-ui-kit-ring-offset-1 has-[:focus]:dgs-ui-kit-ring-offset-white':
-            variant === 'bordered' && isError,
+            variant === 'bordered' && showError,
         },
         containerClassName,
       )}
@@ -73,9 +74,9 @@ const SelectableInputWrapper: FC<SelectableInputWrapperProps> = (props) => {
               'dgs-ui-kit-peer dgs-ui-kit-w-full dgs-ui-kit-h-full disabled:dgs-ui-kit-bg-gray-200 dgs-ui-kit-appearance-none dgs-ui-kit-border dgs-ui-kit-border-gray-300 group-hover:dgs-ui-kit-border-primary-500 checked:dgs-ui-kit-bg-primary-500 checked:dgs-ui-kit-border-transparent dgs-ui-kit-transition-all',
               {
                 'group-hover:dgs-ui-kit-ring-4 group-hover:dgs-ui-kit-ring-gray-100 focus:!dgs-ui-kit-ring focus:!dgs-ui-kit-ring-primary-300 focus:dgs-ui-kit-ring-offset-1 focus:dgs-ui-kit-ring-offset-white':
-                  variant !== 'bordered' && !isError,
+                  variant !== 'bordered' && !showError,
                 '!dgs-ui-kit-border-error-500 group-hover:dgs-ui-kit-ring-4 group-hover:dgs-ui-kit-ring-error-50 focus:!dgs-ui-kit-ring focus:!dgs-ui-kit-ring-error-300 focus:dgs-ui-kit-ring-offset-1 focus:dgs-ui-kit-ring-offset-white':
-                  variant !== 'bordered' && isError,
+                  variant !== 'bordered' && showError,
               },
             ),
             checkedInputClassName:
@@ -87,16 +88,14 @@ const SelectableInputWrapper: FC<SelectableInputWrapperProps> = (props) => {
             <div className="dgs-ui-kit-font-p2-medium dgs-ui-kit-text-gray-700 dgs-ui-kit-transition group-hover:dgs-ui-kit-text-primary-500">
               {label}
             </div>
-            {Boolean(errorMessage ?? helperMessage) && (
+            {Boolean(errorMessage || helperMessage) && (
               <div
                 className={clsx(
                   'dgs-ui-kit-font-p3-regular',
-                  errorMessage && isError
-                    ? 'dgs-ui-kit-text-error-500'
-                    : 'dgs-ui-kit-text-gray-500',
+                  errorMessage ? 'dgs-ui-kit-text-error-500' : 'dgs-ui-kit-text-gray-500',
                 )}
               >
-                {isError && errorMessage ? errorMessage : helperMessage}
+                {errorMessage || helperMessage}
               </div>
             )}
           </div>
