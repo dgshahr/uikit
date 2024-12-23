4288134bb827fdb133dd64b87ffeacc31fa726c9
nazari2079 <mohammadnazary2000@gmail.com>
2024-12-23 13:56:38 +0330

    fix(components): handle scroll on breadcrumb

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -18,18 +18,13 @@ const Breadcrumb = (props: BreadcrumbProps) => {
   const { items, pageTitle, className } = props;
 
   return (
-    <div
-      className={clsx(
-        'dgs-ui-kit-bg-gray-100 dgs-ui-kit-overflow-x-hidden dgs-ui-kit-w-full',
-        className,
-      )}
-    >
-      <div className="dgs-ui-kit-flex dgs-ui-kit-items-center dgs-ui-kit-gap-x-1 dgs-ui-kit-w-full dgs-ui-kit-py-3 dgs-ui-kit-container">
+    <div className={clsx('dgs-ui-kit-bg-gray-100 dgs-ui-kit-w-full', className)}>
+      <div className="dgs-ui-kit-flex dgs-ui-kit-items-center dgs-ui-kit-gap-x-1 dgs-ui-kit-py-3 dgs-ui-kit-overflow-x-auto dgs-ui-kit-container">
         {items.map((breadcrumbItem) => (
           <a
             key={breadcrumbItem.link}
             href={breadcrumbItem.link}
-            className="dgs-ui-kit-transition dgs-ui-kit-flex dgs-ui-kit-items-center dgs-ui-kit-gap-x-1 dgs-ui-kit-text-gray-500 hover:dgs-ui-kit-text-primary-500"
+            className="dgs-ui-kit-transition dgs-ui-kit-shrink-0 dgs-ui-kit-flex dgs-ui-kit-items-center dgs-ui-kit-gap-x-1 dgs-ui-kit-text-gray-500 hover:dgs-ui-kit-text-primary-500"
           >
             {breadcrumbItem.icon && breadcrumbItem.icon}
             <div className="dgs-ui-kit-font-caption-demibold">{breadcrumbItem.title}</div>
@@ -39,7 +34,9 @@ const Breadcrumb = (props: BreadcrumbProps) => {
             />
           </a>
         ))}
-        <div className="dgs-ui-kit-text-gray-500 dgs-ui-kit-font-caption-regular">{pageTitle}</div>
+        <div className="dgs-ui-kit-text-gray-500 dgs-ui-kit-font-caption-regular dgs-ui-kit-shrink-0">
+          {pageTitle}
+        </div>
       </div>
     </div>
   );
