5a28f4486d6f2b5ca71f60a50f8be2e3a32b8e00
nazari2079 <mohammadnazary2000@gmail.com>
2025-01-21 14:39:45 +0330

    feat(utils): add convert number utility

---
+++ src/utils/convertNumbers.ts
@@ -0,0 +1,14 @@
+export const englishToPersian = (s: string): string =>
+  s.replace(/\d/g, (d: string) => '۰۱۲۳۴۵۶۷۸۹'[parseInt(d)] ?? '');
+export const englishToArabic = (s: string): string =>
+  s.replace(/\d/g, (d: string) => '٠١٢٣٤٥٦٧٨٩'[parseInt(d)] ?? '');
+
+export const persianToEnglish = (s: string): string =>
+  s.replace(/[۰-۹]/g, (d: string) => String('۰۱۲۳۴۵۶۷۸۹'.indexOf(d)));
+export const arabicToEnglish = (s: string): string =>
+  s.replace(/[٠-٩]/g, (d: string) => String('٠١٢٣٤٥٦٧٨٩'.indexOf(d)));
+
+export const persianToArabic = (s: string): string =>
+  s.replace(/[۰-۹]/g, (d: string) => '٠١٢٣٤٥٦٧٨٩'['۰۱۲۳۴۵۶۷۸۹'.indexOf(d)] ?? '');
+export const arabicToPersian = (s: string): string =>
+  s.replace(/[٠-٩]/g, (d: string) => '۰۱۲۳۴۵۶۷۸۹'['٠١٢٣٤٥٦٧٨٩'.indexOf(d)] ?? '');
