e647d0b253e2a60d34c9461c7cad6192c2d153b6
nazari2079 <mohammadnazary2000@gmail.com>
2025-02-02 14:59:55 +0330

    docs(storybook): add dana font

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-actions'],
+  staticDirs: ['../assets'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
