0130d993aa32cea225c353f6d7d9d040e3d60244
Alioo11 <110mors110@gmail.com>
2025-03-04 10:34:52 +0330

    fix: removed unused state in stepper context

--- src/components/Stepper/context.ts
+++ src/components/Stepper/context.ts
@@ -3,14 +3,12 @@ import type { StepperSize, StepperStepOrientation } from './type';
 
 interface ContextType {
   activeStep: number;
-  totalSteps: number;
   stepOrientation: StepperStepOrientation;
   size: StepperSize;
 }
 
 export const StepperContext = createContext<ContextType>({
   activeStep: 1,
-  totalSteps: 0,
   stepOrientation: 'horizontal',
   size: 'medium',
 });
@@ -18,9 +16,7 @@ export const StepperContext = createContext<ContextType>({
 export const useStepperContext = () => {
   const context = useContext(StepperContext);
 
-  if (!context) {
-    throw new Error('useStepperContext must be used within a StepperProvider');
-  }
+  if (!context) throw new Error('useStepperContext must be used within a StepperProvider');
 
   return context;
 };
