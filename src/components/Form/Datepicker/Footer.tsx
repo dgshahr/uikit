cf053c96d75b10782072192df12428954862c995
nazari2079 <mohammadnazary2000@gmail.com>
2025-03-18 12:36:57 +0330

    fix(components): add type attribute to Datepicker footer button elements

--- src/components/Form/Datepicker/Footer.tsx
+++ src/components/Form/Datepicker/Footer.tsx
@@ -19,6 +19,7 @@ const Footer: FC = () => {
     >
       {showTodayButton && (
         <Button
+          type="button"
           variant="text"
           size="small"
           isFullWidth={showSubmitButton}
@@ -32,6 +33,7 @@ const Footer: FC = () => {
       )}
       {showSubmitButton && (
         <Button
+          type="button"
           size="small"
           isFullWidth={showTodayButton}
           onClick={() => {
