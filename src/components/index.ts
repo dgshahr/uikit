b91160118a228b3758d9b6dc5b3cbc0beedc3422
nazari2079 <mohammadnazary2000@gmail.com>
2025-04-08 15:42:09 +0330

    feat: add Sidebar component export to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,6 +18,7 @@ export { default as Textarea } from './Form/Textarea';
 export { default as Pagination } from './Pagination';
 export { default as ProgressBar } from './Progress/Bar';
 export { default as ProgressDoughnut } from './Progress/Doughnut';
+export { default as Sidebar } from './Sidebar';
 export { Slider, Slide } from './Slider';
 export { HorizontalStepper, HorizontalStep } from './Stepper/Horizontal';
 export { default as Tabs } from './Tabs';
