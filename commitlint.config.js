ca7d67af0da9ee621618be99d42099ae5bfccbe4
nazari2079 <mohammadnazary2000@gmail.com>
2025-05-11 13:58:38 +0330

    chore(commitlint): remove scope enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,12 @@
 import conventionalCommit from './conventionalCommit.json' assert { type: 'json' };
 
 const typesEnum = Object.keys(conventionalCommit.types);
-const scopesEnum = Object.keys(conventionalCommit.scopes);
 
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', typesEnum],
     'scope-case': [2, 'always', ['camel-case']],
-    'scope-enum': [2, 'always', scopesEnum],
     'scope-empty': [1, 'never'],
     'subject-empty': [2, 'never'],
     'header-max-length': [1, 'always', 72],
