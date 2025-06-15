f946081d8939b599cf74ec89f46973e033c8fee2
nazari2079 <mohammadnazary2000@gmail.com>
2025-06-15 13:08:23 +0330

    feat(icons): add star fill

---
+++ src/icons/IconStarFill.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const IconStarFill = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M10.92 2.868a1.25 1.25 0 0 1 2.16 0l2.795 4.798 5.428 1.176a1.25 1.25 0 0 1 .667 2.054l-3.7 4.141.56 5.525a1.25 1.25 0 0 1-1.748 1.27L12 19.592l-5.082 2.24a1.25 1.25 0 0 1-1.748-1.27l.56-5.525-3.7-4.14a1.25 1.25 0 0 1 .667-2.055l5.428-1.176z"
+    />
+  </svg>
+);
+export default IconStarFill;
