b73c43dd86c76d77ab88b258ad9d374cb81d61c8
nazari2079 <mohammadnazary2000@gmail.com>
2025-06-15 13:07:36 +0330

    fix: change commitlint file to cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,9 @@
-import conventionalCommit from './conventionalCommit.json' assert { type: 'json' };
+/* eslint-disable no-undef */
+const conventionalCommit = require('./conventionalCommit.json');
 
 const typesEnum = Object.keys(conventionalCommit.types);
 
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', typesEnum],
