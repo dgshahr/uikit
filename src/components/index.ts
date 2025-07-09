9ed7ae8de64b0f3b80fc1e09f2cd46b82c7f0f8f
nazari2079 <mohammadnazary2000@gmail.com>
2025-07-09 10:39:31 +0330

    fix(index): add missing Card export to component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Alert } from './Alert';
 export { default as Badge } from './Badge';
 export { default as Breadcrumb } from './Breadcrumb';
 export { default as Button } from './Button';
+export { default as Card } from './Card';
 export { default as Chip } from './Chip';
 export { default as Divider } from './Divider';
 export { default as Drawer } from './Drawer';
