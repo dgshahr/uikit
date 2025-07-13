d3f580e357e527b4d7088edad14f08616a4d50cd
nazari2079 <mohammadnazary2000@gmail.com>
2025-07-13 13:54:32 +0330

    chore(commitlint): add pascal-case for scope

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', typesEnum],
-    'scope-case': [2, 'always', ['camel-case']],
+    'scope-case': [2, 'always', ['camel-case', 'pascal-case']],
     'scope-empty': [1, 'never'],
     'subject-empty': [2, 'never'],
     'header-max-length': [1, 'always', 72],
