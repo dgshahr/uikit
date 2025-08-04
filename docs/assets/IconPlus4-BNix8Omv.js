dea2e88d3851828a928897e89bcf8d2ac0aa0c8b
nazari2079 <mohammadnazary2000@gmail.com>
2025-08-04 15:18:47 +0330

    docs: build storybook

---
+++ docs/assets/IconPlus4-BNix8Omv.js
@@ -0,0 +1,16 @@
+import { j as o } from './jsx-runtime-D_zvdyIk.js';
+const e = (s) =>
+  o.jsx('svg', {
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    ...s,
+    children: o.jsx('path', {
+      fill: 'currentColor',
+      d: 'M12 5a.75.75 0 0 1 .75.75V11H18a.75.75 0 0 1 0 1.5h-5.25v5.25a.75.75 0 0 1-1.5 0V12.5H6A.75.75 0 0 1 6 11h5.25V5.75A.75.75 0 0 1 12 5',
+    }),
+  });
+e.__docgenInfo = { description: '', methods: [], displayName: 'IconPlus4' };
+export { e as I };
