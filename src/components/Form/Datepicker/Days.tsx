e30047555851e37a8b6018dc61bc48aeb6843a62
nazari2079 <mohammadnazary2000@gmail.com>
2025-08-04 12:53:54 +0330

    refactor(DatePicker): move days helper function to utils file and handle disabled and holiday dates in dayItem object

--- src/components/Form/Datepicker/Days.tsx
+++ src/components/Form/Datepicker/Days.tsx
@@ -1,120 +1,16 @@
-import { addDays } from 'date-fns-jalali/addDays';
-import { eachDayOfInterval } from 'date-fns-jalali/eachDayOfInterval';
-import { endOfMonth } from 'date-fns-jalali/endOfMonth';
-import { endOfWeek } from 'date-fns-jalali/endOfWeek';
 import { format } from 'date-fns-jalali/format';
-import { getDay } from 'date-fns-jalali/getDay';
-import { isAfter } from 'date-fns-jalali/isAfter';
 import { isBefore } from 'date-fns-jalali/isBefore';
 import { isSameDay } from 'date-fns-jalali/isSameDay';
-import { isWithinInterval } from 'date-fns-jalali/isWithinInterval';
-import { startOfMonth } from 'date-fns-jalali/startOfMonth';
-import { startOfWeek } from 'date-fns-jalali/startOfWeek';
-import { subDays } from 'date-fns-jalali/subDays';
 import type { FC } from 'react';
 import { useDatepickerContext } from './context';
-import type { DatepickerProps } from './types';
-
-type DayItem = { date: Date; isInMonth: boolean }[];
+import { getDayClassName, getDaysOfCalendar } from './utils';
 
 const SHORT_WEEK_DAYS = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج'];
 
-function getDaysOfCalendar(internalDate: Date) {
-  let tempDays: DayItem = [];
-  const endOfCurrentMonth = endOfMonth(internalDate);
-  const startOfCurrentMonth = startOfMonth(internalDate);
-  const daysOfMonth = eachDayOfInterval({
-    start: startOfMonth(internalDate),
-    end: endOfMonth(internalDate),
-  }).map((date) => ({ date, isInMonth: true }));
-  const startOfMonthWeekday = getDay(startOfCurrentMonth);
-  const endOfMonthWeekday = getDay(endOfCurrentMonth);
-
-  if (startOfMonthWeekday !== 6) {
-    const daysTillStartOfWeek = eachDayOfInterval({
-      start: startOfWeek(startOfCurrentMonth),
-      end: subDays(startOfCurrentMonth, 1),
-    });
-    tempDays = [
-      ...daysTillStartOfWeek.map((date) => ({
-        date,
-        isInMonth: false,
-      })),
-      ...daysOfMonth,
-    ];
-  } else tempDays = [...daysOfMonth];
-
-  if (endOfMonthWeekday != 5) {
-    const daysTillEndOfWeek = eachDayOfInterval({
-      start: addDays(endOfCurrentMonth, 1),
-      end: endOfWeek(endOfCurrentMonth),
-    }).map((date) => ({
-      date,
-      isInMonth: false,
-    }));
-    tempDays = [...tempDays, ...daysTillEndOfWeek];
-  }
-
-  return tempDays;
-}
-
-function getDayClassName({
-  date,
-  isInMonth,
-  showExtraDays,
-  value,
-  startDate,
-  endDate,
-  highlightWeekends,
-  holidays,
-  disableDates,
-}: DayItem[0] & DatepickerProps) {
-  const isDateVisible = isInMonth || showExtraDays;
-  if (!isDateVisible) return 'dgsuikit:pointer-events-none';
-
-  const isToday = isSameDay(new Date(), date);
-  const isWeekend = getDay(date) === 5;
-  const isSelectable =
-    (startDate ? isAfter(date, startDate) : true) && (endDate ? isBefore(date, endDate) : true);
-  const isHoliday = holidays?.length && holidays.some((hDate) => isSameDay(hDate, date));
-  const isDisabled = disableDates?.length && disableDates.some((dDate) => isSameDay(dDate, date));
-
-  const activeItemClass = 'dgsuikit:!bg-primary-500 dgsuikit:!text-white dgsuikit:border-none';
-
-  let className =
-    'dgsuikit:py-1 dgsuikit:rounded-2xl dgsuikit:transition dgsuikit:hover:bg-primary-50 dgsuikit:hover:text-primary-500';
-
-  if (isToday) className = `${className} dgsuikit:border dgsuikit:border-primary-300`;
-  if (!isInMonth && isDateVisible) className = `${className} dgsuikit:text-gray-400`;
-  else if ((isWeekend && highlightWeekends) || isHoliday)
-    className = `${className} dgsuikit:!text-error-500`;
-  else className = `${className} dgsuikit:text-gray-600`;
-  if (!isSelectable) className = `${className} dgsuikit:line-through dgsuikit:pointer-events-none`;
-  if (isDisabled) className = `${className} dgsuikit:bg-gray-200 dgsuikit:pointer-events-none`;
-
-  if (value instanceof Date) {
-    if (isSameDay(value, date)) className = `${className} ${activeItemClass}`;
-  } else if (value.start && isSameDay(value.start, date))
-    className = `${className} dgsuikit:rounded-l-none ${activeItemClass}`;
-  else if (value.end && isSameDay(value.end, date))
-    className = `${className} dgsuikit:rounded-r-none ${activeItemClass}`;
-  else if (
-    value.start &&
-    value.end &&
-    isWithinInterval(date, {
-      start: value.start,
-      end: value.end,
-    })
-  )
-    className = `${className} dgsuikit:rounded-none dgsuikit:border-none dgsuikit:bg-primary-50 dgsuikit:text-primary-500`;
-
-  return className;
-}
-
 const Days: FC = () => {
   const { internalDate, datepickerProps } = useDatepickerContext();
-  const { showExtraDays, onChange, acceptRange, value } = datepickerProps;
-  const daysOfCalendar = getDaysOfCalendar(internalDate);
+  const { showExtraDays, onChange, acceptRange, value, disableDates, holidays } = datepickerProps;
+  const daysOfCalendar = getDaysOfCalendar(internalDate, disableDates, holidays);
 
   function handleChange(date: Date) {
     if (acceptRange) {
@@ -143,6 +39,7 @@ const Days: FC = () => {
             key={dayItem.date.toString()}
             className={getDayClassName({ ...dayItem, ...datepickerProps })}
             onClick={() => handleChange(dayItem.date)}
+            disabled={dayItem.isDisabled}
           >
             {dayItem.isInMonth || showExtraDays ? format(dayItem.date, 'd') : ''}
           </button>
---
+++ src/components/Form/Datepicker/utils.ts
@@ -0,0 +1,117 @@
+import { addDays } from 'date-fns-jalali/addDays';
+import { eachDayOfInterval } from 'date-fns-jalali/eachDayOfInterval';
+import { endOfMonth } from 'date-fns-jalali/endOfMonth';
+import { endOfWeek } from 'date-fns-jalali/endOfWeek';
+import { getDay } from 'date-fns-jalali/getDay';
+import { isAfter } from 'date-fns-jalali/isAfter';
+import { isBefore } from 'date-fns-jalali/isBefore';
+import { isSameDay } from 'date-fns-jalali/isSameDay';
+import { isWithinInterval } from 'date-fns-jalali/isWithinInterval';
+import { startOfMonth } from 'date-fns-jalali/startOfMonth';
+import { startOfWeek } from 'date-fns-jalali/startOfWeek';
+import { subDays } from 'date-fns-jalali/subDays';
+import type { DatepickerProps } from './types';
+
+type DayItem = { date: Date; isInMonth: boolean; isDisabled: boolean; isHoliday: boolean }[];
+
+function isDateInDateArray(datesArray: Date[], date: Date) {
+  return datesArray.some((disabledDate) => isSameDay(disabledDate, date));
+}
+
+export function getDaysOfCalendar(
+  internalDate: Date,
+  disableDates: DatepickerProps['disableDates'] = [],
+  holidays: DatepickerProps['holidays'] = [],
+): DayItem {
+  let tempDays: DayItem = [];
+  const endOfCurrentMonth = endOfMonth(internalDate);
+  const startOfCurrentMonth = startOfMonth(internalDate);
+  const daysOfMonth = eachDayOfInterval({
+    start: startOfMonth(internalDate),
+    end: endOfMonth(internalDate),
+  }).map((date) => ({
+    date,
+    isInMonth: true,
+    isDisabled: isDateInDateArray(disableDates, date),
+    isHoliday: isDateInDateArray(holidays, date),
+  }));
+  const startOfMonthWeekday = getDay(startOfCurrentMonth);
+  const endOfMonthWeekday = getDay(endOfCurrentMonth);
+
+  if (startOfMonthWeekday !== 6) {
+    const daysTillStartOfWeek = eachDayOfInterval({
+      start: startOfWeek(startOfCurrentMonth),
+      end: subDays(startOfCurrentMonth, 1),
+    }).map((date) => ({
+      date,
+      isInMonth: false,
+      isDisabled: isDateInDateArray(disableDates, date),
+      isHoliday: isDateInDateArray(holidays, date),
+    }));
+    tempDays = [...daysTillStartOfWeek, ...daysOfMonth];
+  } else tempDays = [...daysOfMonth];
+
+  if (endOfMonthWeekday != 5) {
+    const daysTillEndOfWeek = eachDayOfInterval({
+      start: addDays(endOfCurrentMonth, 1),
+      end: endOfWeek(endOfCurrentMonth),
+    }).map((date) => ({
+      date,
+      isInMonth: false,
+      isDisabled: isDateInDateArray(disableDates, date),
+      isHoliday: isDateInDateArray(holidays, date),
+    }));
+    tempDays = [...tempDays, ...daysTillEndOfWeek];
+  }
+
+  return tempDays;
+}
+
+export function getDayClassName({
+  date,
+  isInMonth,
+  isHoliday,
+  showExtraDays,
+  value,
+  startDate,
+  endDate,
+  highlightWeekends,
+}: DayItem[0] & DatepickerProps) {
+  const isDateVisible = isInMonth || showExtraDays;
+  if (!isDateVisible) return 'dgsuikit:pointer-events-none';
+
+  const isToday = isSameDay(new Date(), date);
+  const isWeekend = getDay(date) === 5;
+  const isSelectable =
+    (startDate ? isAfter(date, startDate) : true) && (endDate ? isBefore(date, endDate) : true);
+
+  const activeItemClass = 'dgsuikit:!bg-primary-500 dgsuikit:!text-white dgsuikit:border-none';
+
+  let className =
+    'dgsuikit:py-1 dgsuikit:rounded-2xl dgsuikit:transition dgsuikit:hover:bg-primary-50 dgsuikit:not-disabled:hover:text-primary-500 dgsuikit:disabled:bg-gray-200';
+
+  if (isToday) className = `${className} dgsuikit:border dgsuikit:border-primary-300`;
+  if (!isInMonth && isDateVisible) className = `${className} dgsuikit:text-gray-400`;
+  else if ((isWeekend && highlightWeekends) || isHoliday)
+    className = `${className} dgsuikit:!text-error-500`;
+  else className = `${className} dgsuikit:text-gray-600`;
+  if (!isSelectable) className = `${className} dgsuikit:line-through dgsuikit:pointer-events-none`;
+
+  if (value instanceof Date) {
+    if (isSameDay(value, date)) className = `${className} ${activeItemClass}`;
+  } else if (value.start && isSameDay(value.start, date))
+    className = `${className} dgsuikit:rounded-l-none ${activeItemClass}`;
+  else if (value.end && isSameDay(value.end, date))
+    className = `${className} dgsuikit:rounded-r-none ${activeItemClass}`;
+  else if (
+    value.start &&
+    value.end &&
+    isWithinInterval(date, {
+      start: value.start,
+      end: value.end,
+    })
+  )
+    className = `${className} dgsuikit:rounded-none dgsuikit:border-none dgsuikit:bg-primary-50 dgsuikit:text-primary-500`;
+
+  return className;
+}
