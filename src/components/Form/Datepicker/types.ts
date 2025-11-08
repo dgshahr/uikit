82f9b79de36ff65d361beb16775b5f262879269b
Armin Shaikhy Gorjan <62646219+ArminShaikhy@users.noreply.github.com>
2025-11-08 18:09:10 +0330

    Develop (#38)
    * fix(Timepicker): fix race condition bug in debounce (#24)
    
    * fix(Slider): call getNavigationButtons function in Navigation component (#25)
    
    * chore: bump version to 1.8.1
    
    * feat(Chip): add support for 'xsmall' size and 'color' prop (#28)
    
    * feat(FileUploader): add compact mode and new icons for improved file upload experience (#31)
    
    * feat(Badge): add rightIcon and leftIcon props (#30)
    
    * feat(Badge): add rightIcon and leftIcon props
    
    * feat(Badge): add eslint warning error rule for badge icon prop
    
    ---------
    
    Co-authored-by: reza sedaghati <reza.sedaghati@gmail.com>
    
    * fix(Breadcrumb): change default font and padding styles (#29)
    
    Co-authored-by: reza sedaghati <reza.sedaghati@gmail.com>
    
    * fix(Switch): fix disabled property (#27)
    
    Co-authored-by: reza sedaghati <reza.sedaghati@gmail.com>
    
    * chore: build story
    
    * chore: bump version to 1.9.0
    
    * fix: integrate isBrowser utility for improved environment checks across components (#33)
    
    * chore: bump version to 1.9.1
    
    * fix(DateAndTimePicker): update type imports and enhance default props handling (#35)
    
    * fix: update type definitions and imports for Datepicker and Timepicker components (#37)
    
    ---------
    
    Co-authored-by: Siavash Aghazadeh <54579413+30YA@users.noreply.github.com>
    Co-authored-by: Siavash Aghazadeh <siavash.aghazadeh@gmail.com>
    Co-authored-by: Reza-Sedaghati <119964639+R-sedaghati@users.noreply.github.com>
    Co-authored-by: reza sedaghati <reza.sedaghati@gmail.com>

--- src/components/Form/Datepicker/types.ts
+++ src/components/Form/Datepicker/types.ts
@@ -1,6 +1,36 @@
 import type { HasOrNotRange, HasOrNotSubmitButton } from '../types/DateAndTimePicker';
 import type { PickerWrapperProps } from '../Wrappers/PickerWrapper/type';
 
+export interface IRangeDate {
+  start: Date | null;
+  end: Date | null;
+}
+
+interface WithSubmitButton {
+  showSubmitButton?: true;
+  onSubmit?: () => void;
+}
+
+interface WithoutSubmitButton {
+  showSubmitButton?: false;
+  onSubmit?: never;
+}
+
+export interface WithRange {
+  acceptRange: true;
+  value: IRangeDate;
+  onChange: (value: WithRange['value']) => void;
+}
+
+interface WithoutRange {
+  acceptRange?: false;
+  value: Date | null;
+  onChange: (value: Date) => void;
+}
+
+export type HasOrNotSubmitButton = WithSubmitButton | WithoutSubmitButton;
+export type HasOrNotRange = WithRange | WithoutRange;
+
 export enum DateTypes {
   Day,
   Month,
